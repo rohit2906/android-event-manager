53f89e061f85e4ec2aa7ddbb160707dcdbeb167b
daniele.vitali89 <daniele.vitali89@gmail.com>
2012-03-07 16:02:32 +0000

    Implementation of the login and credentials activities

---
+++ src/code/eventmanager/CredentialsActivity.java
@@ -0,0 +1,16 @@
+package code.eventmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CredentialsActivity extends Activity {
+
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.credentials_layout);
+	}
+
+}
