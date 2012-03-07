8f30d8ce6bf2fc2910a3401b73cc8ee53f470cb4
daniele.vitali89@gmail.com <daniele.vitali89@gmail.com@0622cc9a-d54f-2636-e534-1802e2086dda>
2012-03-07 08:39:11 +0000

    Fisrt commit

---
+++ src/code/eventmanager/EventManagerActivity.java
@@ -0,0 +1,13 @@
+package code.eventmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EventManagerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
