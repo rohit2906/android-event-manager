ad96dedc2391a2b83d7172cd6d5018adb0d13773
daniele.vitali89 <daniele.vitali89@gmail.com>
2012-03-07 22:57:03 +0000

    Start of the eventsActivity and the PollerService

--- src/code/eventmanager/CredentialsActivity.java
+++ src/code/eventmanager/CredentialsActivity.java
@@ -7,27 +7,34 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CredentialsActivity extends Activity implements OnClickListener {
 
 	Button btnLogin;
-	EditText tvUsername;
-	EditText tvPassword;
+	EditText etUsername;
+	EditText etPassword;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.credentials_layout);
 		btnLogin = (Button) findViewById(R.id.credentialsBtnLogin);
-		tvUsername = (EditText) findViewById(R.id.credentialsEtUsername);
-		tvPassword = (EditText) findViewById(R.id.credentialsEtPassword);
+		etUsername = (EditText) findViewById(R.id.credentialsEtUsername);
+		etPassword = (EditText) findViewById(R.id.credentialsEtPassword);
 		btnLogin.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
-		if (tvUsername.getText().toString() != "" && tvPassword.getText().toString() != "")
+		if (etUsername.getText().toString().equalsIgnoreCase(""))
+			Toast.makeText(this, "Write your username ", Toast.LENGTH_LONG).show();
+			else if( etPassword.getText().toString().equalsIgnoreCase(""))
+				Toast.makeText(this, "Write your password ", Toast.LENGTH_LONG).show();
+		else{
+			startService(new Intent(this, PollerService.class));
 			startActivity(new Intent(this, EventsActivity.class));
+		}
+			
 	}
-
 }
