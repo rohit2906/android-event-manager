1e25058d5ff10fee4c4f14e8794a49fe2e45aae0
daniele.vitali89@gmail.com <daniele.vitali89@gmail.com@0622cc9a-d54f-2636-e534-1802e2086dda>
2012-03-07 18:30:30 +0000

    Implementation of CredentialActivity

--- src/code/eventmanager/CredentialsActivity.java
+++ src/code/eventmanager/CredentialsActivity.java
@@ -1,16 +1,33 @@
 package code.eventmanager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class CredentialsActivity extends Activity {
+public class CredentialsActivity extends Activity implements OnClickListener {
 
-	
-	
+	Button btnLogin;
+	EditText tvUsername;
+	EditText tvPassword;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.credentials_layout);
+		btnLogin = (Button) findViewById(R.id.credentialsBtnLogin);
+		tvUsername = (EditText) findViewById(R.id.credentialsEtUsername);
+		tvPassword = (EditText) findViewById(R.id.credentialsEtPassword);
+		btnLogin.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (tvUsername.getText().toString() != "" && tvPassword.getText().toString() != "")
+			startActivity(new Intent(this, EventsActivity.class));
 	}
 
 }
