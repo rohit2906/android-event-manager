085bc1c57e599f751be29e38414dfc670867a0a8
daniele.vitali89@gmail.com <daniele.vitali89@gmail.com@0622cc9a-d54f-2636-e534-1802e2086dda>
2012-03-07 16:02:32 +0000

    Implementation of the login and credentials activities

---
+++ src/code/eventmanager/LoginActivity.java
@@ -0,0 +1,39 @@
+package code.eventmanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class LoginActivity extends Activity implements OnClickListener {
+
+	Button btnLogin;
+	Button btnOtherAccount;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login_layout);
+		btnLogin = (Button) findViewById(R.id.loginBtnLogin);
+		btnOtherAccount = (Button) findViewById(R.id.loginBtnOtherAccount);
+		btnLogin.setOnClickListener(this);
+		btnOtherAccount.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.loginBtnLogin:
+			startActivity(new Intent(this, EventsActivity.class));
+			break;
+		case R.id.loginBtnOtherAccount:
+			startActivity(new Intent(this, CredentialsActivity.class));
+			break;
+
+		default:
+			break;
+		}
+	}
+}
