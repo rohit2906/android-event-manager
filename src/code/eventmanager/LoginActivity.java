7d11606d928a05cba39830339f3291bbbb34d0c3
enrico.bacis <enrico.bacis@gmail.com>
2012-03-08 12:05:55 +0000

    Moved Service code to the proper place

--- src/code/eventmanager/LoginActivity.java
+++ src/code/eventmanager/LoginActivity.java
@@ -3,44 +3,33 @@ package code.eventmanager;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class LoginActivity extends Activity implements OnClickListener {
 	
+	@SuppressWarnings("unused")
 	private static final String TAG = "LoginActivity";
 	
 	Button btnLogin;
 	Button btnOtherAccount;
-	Intent pollerServiceIntent;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_layout);
 		
-		pollerServiceIntent = null;
-		
 		btnLogin = (Button) findViewById(R.id.loginBtnLogin);
 		btnOtherAccount = (Button) findViewById(R.id.loginBtnOtherAccount);
 		btnLogin.setOnClickListener(this);
 		btnOtherAccount.setOnClickListener(this);
-	}
-	
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		// TODO: remove later, it's just for testing on devices
-		stopPoller();
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.loginBtnLogin:
-			startPoller();
 			startActivity(new Intent(this, EventsActivity.class));
 			break;
 			
@@ -50,24 +39,6 @@ public class LoginActivity extends Activity implements OnClickListener {
 			
 		default:
 			break;
-		}
-	}
-	
-	private void startPoller() {
-		if (pollerServiceIntent == null) {
-			pollerServiceIntent = new Intent(this, PollerService.class);
-			startService(pollerServiceIntent);
-		} else {
-			Log.d(TAG, "startPoller(): PollerService is already running.");
-		}
-	}
-	
-	private void stopPoller() {
-		if (pollerServiceIntent != null) {
-			stopService(pollerServiceIntent);
-			pollerServiceIntent = null;
-		} else {
-			Log.d(TAG, "stopPoller(): PollerService is not running.");
 		}
 	}
 }
