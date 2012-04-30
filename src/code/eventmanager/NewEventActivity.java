c46910b46e1145b88769127347b288a4dab3dadf
theo.merou@gmail.com <theo.merou@gmail.com@0622cc9a-d54f-2636-e534-1802e2086dda>
2012-04-30 16:56:30 +0000

    NewEventActivity

--- src/code/eventmanager/NewEventActivity.java
+++ src/code/eventmanager/NewEventActivity.java
@@ -1,5 +1,9 @@
 package code.eventmanager;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
@@ -26,12 +30,17 @@ public class NewEventActivity extends Activity implements OnClickListener {
 	@SuppressWarnings("unused")
 	private static final String TAG = NewEventActivity.class.getSimpleName();
 
+	EventManagerApp app;
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		
 		Log.d(TAG, "on create");
 		setContentView(R.layout.newevent_layout);
+		app = (EventManagerApp) getApplication();
 		
 		titleNewEvent = (EditText) findViewById(R.id.neweventEditTextTitle);
 		descriptionNewEvent = (EditText) findViewById(R.id.newEventEditTextDescription);
@@ -41,10 +50,28 @@ public class NewEventActivity extends Activity implements OnClickListener {
 		ending_hourNewEvent = (EditText) findViewById(R.id.neweventEditTextEndingHour);
 		create_buttonNewEvent = (Button) findViewById(R.id.neweventCreateButton);
 		
+		create_buttonNewEvent.setOnClickListener(this);
 		dbHelper = new DbHelper(this);
 		
 	}
+	
+	public long getTimeStamp(String myString)
+	{
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+		try
+		{
+			Date parsedDate = dateFormat.parse(myString);
+			Timestamp timestamp = new Timestamp(parsedDate.getTime());
+			long time = timestamp.getTime();
+			return time;
+		}catch (Exception e) {
+			Log.d("TAG", "Non valable date format");
+			return -1;
+		}
+		
+	}
 
+	
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
@@ -52,13 +79,11 @@ public class NewEventActivity extends Activity implements OnClickListener {
 		SQLiteDatabase db = dbHelper.getWritableDatabase(); //open the database
 		ContentValues record = new ContentValues();
 		
-		record.put(DbHelper.EVENTS_NAME, titleNewEvent);
-		record.put(DbHelper.EVENTS_DESCRIPTION, descriptionNewEvent);
-		record.put(DbHelper.EVENTS_CREATOR, wsc.get(3).getValue());
-		record.put(DbHelper.EVENTS_STARTING_TS,
-				Integer.parseInt(wsc.get(3).getValue()));
-		record.put(DbHelper.EVENTS_ENDING_TS,
-				Integer.parseInt(wsc.get(4).getValue()));
+		record.put(DbHelper.EVENTS_NAME, titleNewEvent.getText().toString());
+		record.put(DbHelper.EVENTS_DESCRIPTION, descriptionNewEvent.getText().toString());
+		//record.put(DbHelper.EVENTS_CREATOR, app.getSpreadsheetFactory().);
+		record.put(DbHelper.EVENTS_STARTING_TS, getTimeStamp(starting_hourNewEvent.getText().toString()));
+		record.put(DbHelper.EVENTS_ENDING_TS, getTimeStamp(ending_hourNewEvent.getText().toString()));
 	}
 	
 	
