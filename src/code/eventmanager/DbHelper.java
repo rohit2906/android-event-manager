669e9989e2a4d7ca78275232368919ee18c9db2e
daniele.vitali89@gmail.com <daniele.vitali89@gmail.com@0622cc9a-d54f-2636-e534-1802e2086dda>
2012-05-01 14:19:16 +0000

    Restyling of the newevent_layout.

--- src/code/eventmanager/DbHelper.java
+++ src/code/eventmanager/DbHelper.java
@@ -14,8 +14,9 @@ public class DbHelper extends SQLiteOpenHelper {
 	public static int DB_VERSION = 1;
 	public static final String TABLE_EVENTS = "events";
 	public static final String TABLE_ATTENDINGS = "attendings";
-	public static final String EVENTS_ID = BaseColumns._ID;			//why???
+	public static final String EVENTS_ID = BaseColumns._ID; // why???
 	public static final String EVENTS_NAME = "name";
+	public static final String EVENTS_ADDRESS = "address";
 	public static final String EVENTS_DESCRIPTION = "description";
 	public static final String EVENTS_CREATOR = "creator";
 	public static final String EVENTS_STARTING_TS = "starting_timestamp";
@@ -38,17 +39,18 @@ public class DbHelper extends SQLiteOpenHelper {
 		String sql;
 		sql = "CREATE TABLE " + TABLE_EVENTS + " (" + EVENTS_ID
 				+ " integer NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, "
-				+ EVENTS_NAME + " text NOT NULL, " + EVENTS_DESCRIPTION
-				+ " text, " + EVENTS_CREATOR + " text NOT NULL, "
-				+ EVENTS_STARTING_TS + " integer NOT NULL, " + EVENTS_ENDING_TS
+				+ EVENTS_NAME + " text NOT NULL, " + EVENTS_ADDRESS + " text, "
+				+ EVENTS_DESCRIPTION + " text, " + EVENTS_CREATOR
+				+ " text NOT NULL, " + EVENTS_STARTING_TS
+				+ " integer NOT NULL, " + EVENTS_ENDING_TS
 				+ " integer NOT NULL)";
 		db.execSQL(sql);
 		Log.v(TAG, "SQL executed: " + sql);
 		sql = "CREATE TABLE " + TABLE_ATTENDINGS + " (" + ATTENDINGS_EVENT_ID
 				+ " integer NOT NULL REFERENCES " + TABLE_EVENTS + "("
 				+ EVENTS_ID + ") ON DELETE CASCADE ON UPDATE CASCADE, "
-				+ ATTENDINGS_EMAILS
-				+ " text NOT NULL, PRIMARY KEY (" + ATTENDINGS_EVENT_ID + "," + ATTENDINGS_EMAILS + "))";
+				+ ATTENDINGS_EMAILS + " text NOT NULL, PRIMARY KEY ("
+				+ ATTENDINGS_EVENT_ID + "," + ATTENDINGS_EMAILS + "))";
 		db.execSQL(sql);
 		Log.v(TAG, "SQL executed: " + sql);
 	}
