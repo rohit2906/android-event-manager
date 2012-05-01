5139c258cacc43e5dccd3a08344e7b81b93cb7a3
daniele.vitali89@gmail.com <daniele.vitali89@gmail.com@0622cc9a-d54f-2636-e534-1802e2086dda>
2012-05-01 18:27:04 +0000

    Bugs fixed, new event tested

--- src/code/eventmanager/NewEventActivity.java
+++ src/code/eventmanager/NewEventActivity.java
@@ -211,27 +211,26 @@ public class NewEventActivity extends Activity implements OnClickListener {
 		c.set(Calendar.MILLISECOND, 0);
 		return c.getTimeInMillis();
 	}
-	
-	
-	
-	private String getCreator()
-	{
+
+	private String getCreator() {
 		String creator;
-		boolean checked = app.getPrefs().getBoolean(getText(R.string.credentialsKeyDefaultAccount).toString(), true);
-		
-		if(checked)
-		{
-			AccountManager manager = AccountManager.get(getApplicationContext());
+		boolean checked = app.getPrefs()
+				.getBoolean(
+						getText(R.string.credentialsKeyDefaultAccount)
+								.toString(), true);
+
+		if (checked) {
+			AccountManager manager = AccountManager
+					.get(getApplicationContext());
 			creator = manager.getAccountsByType("com.google")[0].name;
+		} else {
+			creator = app.getPrefs().getString(
+					getText(R.string.credentialsKeyCustomAccountMail)
+							.toString(), "");
 		}
-		else
-		{
-			creator = app.getPrefs().getString(getText(R.string.credentialsKeyCustomAccountMail).toString(), "");
-		}
-		
+
 		return creator;
 	}
-	
 
 	/**
 	 * catch the click of the dates, times, create buttons
@@ -253,105 +252,113 @@ public class NewEventActivity extends Activity implements OnClickListener {
 			showDialog(ENDING_TIME_DIALOG_ID);
 			break;
 		case R.id.neweventButtonCreate:
-			
-			
-			SQLiteDatabase db = app.getDbHelper().getWritableDatabase(); // open the database
-			ContentValues record = new ContentValues();
-			
-			  record.put(DbHelper.EVENTS_NAME,
-					  	etTitle.getText().toString());
 
-			  record.put(DbHelper.EVENTS_ADDRESS, etAddress.getText().toString());
-			
-			  record.put(DbHelper.EVENTS_DESCRIPTION,
-			  etDescription.getText().toString());//
-			  
-			  record.put(DbHelper.EVENTS_CREATOR,
-					  getCreator());
-			  
-			  record.put(DbHelper.EVENTS_STARTING_TS,
-			  date2Timestamp(starting.getYear(), starting.getMonth(), starting.getDate(), starting.getHours(), starting.getMinutes()));
-			  
-			  record.put(DbHelper.EVENTS_ENDING_TS,
-			  date2Timestamp(ending.getYear(), ending.getMonth(), ending.getDate(), ending.getHours(), ending.getMinutes()));
-			 
-			  boolean cnt = true;
-			  
-			  try {
-					db.insertOrThrow(DbHelper.TABLE_EVENTS, null, record); // insert
-																			// the
-																			// record
-																			// into
+			SQLiteDatabase db = app.getDbHelper().getWritableDatabase(); // open
 																			// the
 																			// database
-					Log.v(TAG, "Record inserted");
-					
-				} catch (SQLException e) {
-					Log.w(TAG, "Record not inserted");
-					cnt = false;
+			ContentValues record = new ContentValues();
+
+			record.put(DbHelper.EVENTS_NAME, etTitle.getText().toString());
+
+			record.put(DbHelper.EVENTS_ADDRESS, etAddress.getText().toString());
+
+			record.put(DbHelper.EVENTS_DESCRIPTION, etDescription.getText()
+					.toString());//
+
+			record.put(DbHelper.EVENTS_CREATOR, getCreator());
+
+			record.put(
+					DbHelper.EVENTS_STARTING_TS,
+					new Long(date2Timestamp(starting.getYear(),
+							starting.getMonth(), starting.getDate(),
+							starting.getHours(), starting.getMinutes()))
+							.toString());
+
+			record.put(
+					DbHelper.EVENTS_ENDING_TS,
+					new Long(date2Timestamp(ending.getYear(),
+							ending.getMonth(), ending.getDate(),
+							ending.getHours(), ending.getMinutes())).toString());
+
+			boolean cnt = true;
+
+			try {
+				db.insertOrThrow(DbHelper.TABLE_EVENTS, null, record); // insert
+																		// the
+																		// record
+																		// into
+																		// the
+																		// database
+				Log.v(TAG, "Record inserted");
+
+			} catch (SQLException e) {
+				Log.w(TAG, "Record not inserted");
+				cnt = false;
+			}
+
+			if (cnt) {
+				record.clear();
+
+				String[] entry = new String[7];
+				String query = "SELECT * FROM " + DbHelper.TABLE_EVENTS
+						+ " WHERE " + DbHelper.EVENTS_ID + " IN (SELECT MAX("
+						+ DbHelper.EVENTS_ID + ") FROM "
+						+ DbHelper.TABLE_EVENTS + ")";
+				Cursor cursor = db.rawQuery(query, null);
+
+				if (cursor.moveToFirst()) {
+					entry[0] = new Integer(cursor.getInt(0)).toString();
+					entry[1] = cursor.getString(1);
+					entry[2] = cursor.getString(2);
+					entry[3] = cursor.getString(3);
+					entry[4] = cursor.getString(4);
+					entry[5] = cursor.getString(5);
+					entry[6] = new Long(cursor.getLong(6)).toString();
+
+					new UpdateSpreadsheet().execute(entry);
 				}
-				
-				db.close();
-				
-				if(cnt)
-				{
-					db = app.getDbHelper().getReadableDatabase();
-					record.clear();
-					
-					String[] entry = new String[7];
-					String query = "SELECT * FROM "+DbHelper.TABLE_EVENTS+" WHERE "+DbHelper.EVENTS_ID+"= MAX("+DbHelper.EVENTS_ID+")";
-					Cursor cursor = db.rawQuery(query, null);
-					
-					if(cursor.moveToFirst())
-					{
-						entry[0] = new Integer(cursor.getInt(0)).toString();
-						entry[1] = cursor.getString(1);
-						entry[2] = cursor.getString(2);
-						entry[3] = cursor.getString(3);
-						entry[4] = cursor.getString(4);
-						entry[5] = cursor.getString(5);
-						entry[6] = new Long(cursor.getLong(6)).toString();
-						
-						new UpdateSpreadsheet().execute(entry);
-					}
-					
-				}
-				
+
+			}
+
+			db.close();
+			finish();
 			break;
 		case R.id.neweventButtonCancel:
 			finish();
+			break;
 		default:
 			break;
 		}
 	}
-	
-	class UpdateSpreadsheet extends AsyncTask<String, String, String>
-	{
+
+	class UpdateSpreadsheet extends AsyncTask<String, String, String> {
 
 		@Override
 		protected String doInBackground(String... params) {
 
-			String spreadsheetTitle = app.getPrefs().getString((String) getText(R.string.credentialsKeySpreadsheetTitle), "event_manager");
-			ArrayList<SpreadSheet> spreadsheets = app.getSpreadsheetFactory().getSpreadSheet(spreadsheetTitle, false);
+			String spreadsheetTitle = app.getPrefs().getString(
+					(String) getText(R.string.credentialsKeySpreadsheetTitle),
+					"event_manager");
+			ArrayList<SpreadSheet> spreadsheets = app.getSpreadsheetFactory()
+					.getSpreadSheet(spreadsheetTitle, false);
 			WorkSheet ws = spreadsheets.get(0).getAllWorkSheets().get(0);
 			HashMap<String, String> entry = new HashMap<String, String>();
-			entry.put("A", params[0]);
-			entry.put("B", params[1]);
-			entry.put("C", params[2]);
-			entry.put("D", params[3]);
-			entry.put("E", params[4]);
-			entry.put("F", params[5]);
-			entry.put("G", params[6]);
+			entry.put(DbHelper.EVENTS_ID, params[0]);
+			entry.put(DbHelper.EVENTS_NAME, params[1]);
+			entry.put(DbHelper.EVENTS_ADDRESS, params[2]);
+			entry.put(DbHelper.EVENTS_DESCRIPTION, params[3]);
+			entry.put(DbHelper.EVENTS_CREATOR, params[4]);
+			entry.put(DbHelper.EVENTS_STARTING_TS, params[5]);
+			entry.put(DbHelper.EVENTS_ENDING_TS, params[6]);
 			ws.addRecord(spreadsheets.get(0).getKey(), entry);
-			
 			return params[0];
 		}
 
 		@Override
 		protected void onPostExecute(String result) {
-			Toast.makeText(NewEventActivity.this, "spreadsheet updated", Toast.LENGTH_LONG).show();
-		}		
-		
-		
+			Toast.makeText(NewEventActivity.this, "spreadsheet updated",
+					Toast.LENGTH_LONG).show();
+		}
+
 	}
 }
